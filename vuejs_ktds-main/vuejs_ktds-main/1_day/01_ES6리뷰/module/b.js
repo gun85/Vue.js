62c52739abdd235eea66c0a87237b738198c9090
gun85 <programmergun@gmail.com>
2022-01-23 16:27:51 +0900

    test

---
+++ vuejs_ktds-main/vuejs_ktds-main/1_day/01_ES6리뷰/module/b.js
@@ -0,0 +1,8 @@
+//b.js
+
+// export default ()=>{
+//     console.log("arrow fun");
+// }
+// export default  str = "hello";
+const str = "hello";
+export default str;
---
+++ vuejs_ktds-main/vuejs_ktds-main/1_day/01_ES6리뷰/module/c.js
@@ -0,0 +1,8 @@
+// c.js
+import {num,fun,Person} from './a.js';
+import  str   from './b.js';
+console.log(num);
+fun();
+var p = new Person();
+
+console.log(str);
